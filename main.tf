b235defaf6b1fd676a0b2efd83dd22d0066686bb
MayahNaths <mayahnaths002@yahoo.com>
2021-11-26 16:40:26 -0600

    added a subnet and it variables

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
 resource "aws_vpc" "friday_vpc" {
     cidr_block = var.vpcCIDR
 }
+
+resource "aws_subnet" "friday_subnet_1" {
+    availability_zone = var.AZ
+    cidr_block =  var.SN-1-cidr
+    vpc_id = aws_vpc.friday_vpc.id
+}
--- var.tf
+++ var.tf
@@ -1,3 +1,10 @@
 variable "vpcCIDR" {
     default = "10.0.0.0/16"
 }
+
+variable "AZ" {
+    default = "us-east-2" 
+}
+variable "SN-1-cidr" {
+    default = "10.0.3.0/24"  
+}
